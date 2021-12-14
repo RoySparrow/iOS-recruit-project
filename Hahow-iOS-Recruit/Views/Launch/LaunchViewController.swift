81db9eb372dcef72d4106acb819e41cf9d6cf256
Roy Sparrow <rvxl2013@gmail.com>
2021-12-14 16:34:34 +0800

    modify: 1. modify build and bundle version 2. use LaunchScreen as first screen 3. use LaunchVC instead of VC 4. remove unused scripts

---
+++ Hahow-iOS-Recruit/Views/Launch/LaunchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LaunchViewController.swift
+//  Hahow-iOS-Recruit
+//
+//  Created by Roy Sparrow on 2021/12/14.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+    deinit {
+        print("deinit.")
+    }
+}
